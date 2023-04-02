9e8aaed614657a9725c8887d3e6f22878b3572f7
Ammar <me@testing.com>
2023-04-01 19:06:30 -0700

    jwaaaan

---
+++ 0x00-shell_basics/1-swap.c
@@ -0,0 +1,18 @@
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: Pointer to an integer
+ * @b: Pointer to an integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+
+
+
